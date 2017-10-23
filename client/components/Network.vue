d4715e94be35beab6c4627f9ee0991f581fdf45a
Fidel Thomet <fidelthomet@gmail.com>
2017-10-23 19:14:17 +0200

    update for viewing mode

--- client/components/Network.vue
+++ client/components/Network.vue
@@ -1,56 +1,44 @@
 <template>
   <div class="network">
-    <svg width="100%" height="100%" ref="all">
-      <!-- <filter id="blur" x="-50%" y="-50%" width="200%" height="200%">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="16"/>
-      </filter>
-      <filter id="blur-text">
-        <feGaussianBlur in="SourceAlpha" :stdDeviation="3 * zoom" />
-        <feMerge>
-            <feMergeNode />
-            <feMergeNode in="SourceGraphic" />
-        </feMerge>
-      </filter>
-      <filter id="blur-s" x="-50%" y="-50%" width="200%" height="200%">
-        <feGaussianBlur in="SourceGraphic" :stdDeviation="4 / zoom" />
-      </filter>
-      <filter id="blur-line" x="-50%" y="-50%" width="200%" height="200%">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="1" />
-      </filter>
-      <radialGradient id="pink">
-        <stop offset="0%" stop-color="rgba(255, 29, 136, 1)"/>
-        <stop offset="100%" stop-color="rgba(255, 29, 136, 0)"/>
-      </radialGradient> -->
+    <svg width="100%" height="100%" ref="all" @mousemove="mousemove">
       <radialGradient id="yellow">
         <stop offset="0%" stop-opacity="1" stop-color="rgba(255,255,255, 1)"/>
         <stop offset="100%" stop-opacity="0" stop-color="rgba(255,255,255, 1)"/>
       </radialGradient>
+      <radialGradient id="gradient">
+        <stop offset="0%" stop-opacity="0.5" stop-color="rgba(255,255,255, 1)"/>
+        <stop offset="100%" stop-opacity="0" stop-color="rgba(255,255,255, 1)"/>
+      </radialGradient>
       <g :transform="transform" ref="group">
+        <transition name="fade">
+        <g v-if="visible">
         <g>
           <!-- <path v-for="link in elLinks" :class="link.type" :d="`M${link.source.x} ${link.source.y} Q ${width/2} ${height/2} ${link.target.x} ${link.target.y}`" stroke="black" fill="transparent"/> -->
-          <line v-for="link in elLinks" :style="{opacity: focused === null ? 0.2 : (focused._id === link.target._id || focused._id === link.source._id) ? 1 : 0.05 }" :stroke-width="1 / zoom" :class="link.type" :x1="link.source.x" :y1="link.source.y" :x2="link.target.x" :y2="link.target.y"/>
+          <line v-for="link in elLinks" v-if="(focused === null && showLines) || (focused !== null && (focused._id === link.target._id || focused._id === link.source._id))" :style="{opacity: focused === null ? 0.1 : 0.75}" :stroke-width="strokeWidth" :class="link.type" :x1="link.source.x" :y1="link.source.y" :x2="link.target.x" :y2="link.target.y"/>
         </g>
         <g>
-          <circle class="statement-circle" :fill="node.fill" :style="{opacity: node.opacity}" v-for="node in elNodes" v-if="node.type !== 'user'" @click="focus(node)" @mouseover="highlight(node)" @mouseleave="highlight(null)" :r="((12 * node.controversityNorm) || 4) / zoom" :cx="node.x || 0" :cy="node.y || 0"/>
+          <circle class="statement-circle" :fill="node.fill" :style="{opacity: node.opacity}" v-for="node in elNodes" v-if="node.type !== 'user'" @click="focus(node)" @mouseover="highlight(node)" @mouseleave="highlight(null)" :r="((node.controversityNorm12) || 4) / throttledZoom" :cx="node.x || 0" :cy="node.y || 0"/>
         </g>
         <g>
           <g v-for="node in elNodes" v-if="node.type === 'user'" @mouseover="text = node.text" @mousleave="text = null" :transform="'translate(' + (node.x || 0) + ' ' + (node.y || 0) + ')'">
-            <circle :style="{opacity: node.opacity}" stroke="url(#yellow)" class="user-circle" :r="node.r * 64 / zoom" :stroke-width="node.r * 64 / zoom" @mouseover="highlight(node)" @mouseleave="highlight(null)"/>
+            <circle :style="{opacity: node.opacity}" stroke="url(#yellow)" class="user-circle" :r="node.r * 64 / throttledZoom" :stroke-width="node.r * 64 / throttledZoom" @mouseover="highlight(node)" @mouseleave="highlight(null)"/>
+            <!-- <circle v-else :style="{opacity: node.opacity}" fill="url(#gradient)" class="user-circle-filled" :r="8  / throttledZoom" :stroke-width="0" @mouseover="highlight(node)" @mouseleave="highlight(null)"/> -->
             <g class="text" :class="{'right-align': node.textAlign === 'right' }" :transform="`rotate(${node.textDeg || 0})`">
-              <text :style="{'font-size': `${node.r * 40 / zoom}px`, opacity: node.opacity}" class="user" filte="url(#blur-text)" :y="node.r * 10 / zoom" :x="(node.textAlign !== 'right' ? (node.r * 32 + 8) : (-node.r * 32 - 8)) / zoom">{{node.text}}</text>
-              <!-- <text :style="{'font-size': `${node.r * 40 / zoom}px`, opacity: node.opacity}" class="user" :y="node.r * 12 / zoom" :x="(node.textAlign !== 'right' ? (node.r * 32 + 8) : (-node.r * 32 - 8)) / zoom">{{node.text}}</text> -->
+              <text :style="{'font-size': `${node.fontSize * 40 / throttledZoom}px`, opacity: (node.engaged || focused != null) ? node.opacity : 0.4}" class="user" filte="url(#blur-text)" :y="node.r * 10 / throttledZoom" :x="(node.textAlign !== 'right' ? (node.r * 32 + 8) : (-node.r * 32 - 8)) / throttledZoom">{{node.text}}</text>
             </g>
           </g>
         </g>
       </g>
+      </transition>
+      </g>
     </svg>
     <br>
     <transition name="fade">
-      <div class="focus" v-if="focused && focused.type === 'statement' && ((zoom > settings.maxZoom - 0.2 && settings.autoZoom !== 'off') || clicked)" :style="{'border-color': focused.fill, color: focused.fill}" @click="blur">
+      <div class="focus" v-if="focused && focused.type === 'statement' && ((throttledZoom > settings.maxZoom - 0.2 && settings.autoZoom !== 'off') || clicked)" :style="{'border-color': focused.fill, color: focused.fill}" @click="blur">
         <div class="text">
-          <!-- <span class="author">{{focused.ratio}}</span> -->
+          <span class="author">{{focused.author}}:</span>
+          <br>
           {{focused.text}}
-          <span class="author">–&nbsp;{{focused.author}}</span>
         </div>
       </div>
     </transition>
@@ -59,6 +47,7 @@
 
 <script>
 import * as d3 from 'd3'
+import _ from 'lodash'
 
 export default {
   props: ['links', 'nodes', 'settings'],
@@ -74,22 +63,25 @@ export default {
       zoom: 1,
       focused: null,
       d3Zoom: d3.zoom().on('zoom', this.zoomed).on('end', this.zoomended),
-      skipTick: true,
       auto: true,
       autoPause: 2000,
       lastFocus: null,
-      clicked: false
+      clicked: false,
+      visible: false,
+      throttledZoom: 1,
+      showLines: true
     }
   },
   mounted () {
     this.simulation
       .nodes(this.elNodes)
-      .on('tick', this.ticked)
+      .on('end', this.ticked)
       .force('center', d3.forceCenter(this.width / 2, this.height / 2))
 
     this.simulation.force('link')
       .links(this.elLinks)
-    this.simulation.alphaTarget(0).restart()
+    // this.simulation.alphaTarget(0.5)
+    // .restart()
 
     this.onWindowResize(() => {
       let vs = this.getViewportSize()
@@ -98,6 +90,7 @@ export default {
 
       this.simulation.force('center', d3.forceCenter(vs.width / 2, vs.height / 2))
 
+      this.ticked()
       // this.simulation
       //   .nodes(this.elNodes)
       //
@@ -116,9 +109,11 @@ export default {
       })
       .on('mouseup.end dblclick.end wheel.end touchstart.end touchend.end touchcancel.end', null)
 
-    if (this.settings.autoZoom !== 'off') {
-      setTimeout(this.zoomIn, 2000)
-    }
+    setTimeout(() => {
+      this.resetZoom()
+      let statements = this.elNodes.filter(n => n.type === 'statement')
+      this.focus(statements[Math.floor(Math.random() * statements.length)])
+    }, 100)
   },
   methods: {
     ticked () {
@@ -136,13 +131,34 @@ export default {
         })
 
       this.$forceUpdate()
+      this.visible = true
+      this.blur()
     },
+    mousemove: _.throttle(
+      function (e) {
+        // if (this.visible === false) return
+        // this.nodes
+        //   .filter(node => node.type === 'user')
+        //   .forEach(node => {
+        //     let textDeg = Math.atan2(node.y - (e.clientY), node.x - (e.clientX)) * 180 / Math.PI
+        //     if (textDeg < -90 || textDeg > 90) {
+        //       node.textDeg = textDeg + 180
+        //       node.textAlign = 'right'
+        //     } else {
+        //       node.textDeg = textDeg
+        //       node.textAlign = 'left'
+        //     }
+        //   })
+        // this.$forceUpdate()
+      }, 30),
     highlight (node) {
+      if (this.showLines === false) return
       if (this.clicked && node == null) return
       if (this.settings.autoZoom === 'off') {
         this.focused = node
+        let linkedLinks = this.focused == null ? [] : this.links.filter((link) => this.focused._id === link.target._id || this.focused._id === link.source._id)
         this.nodes.forEach(n => {
-          n.opacity = this.checkOpacity(n)
+          n.opacity = this.checkOpacity(n, linkedLinks)
         })
       }
     },
@@ -153,49 +169,53 @@ export default {
         n.opacity = 1
       })
     },
-    checkOpacity (node) {
+    checkOpacity (node, links) {
       if (this.focused == null || this.focused._id === node._id) return 1
 
-      let linked = this.elLinks.find(link => (this.focused._id === link.target._id && node._id === link.source._id) || (this.focused._id === link.source._id && node._id === link.target._id))
+      let linked = links.find(link => (node._id === link.source._id) || (node._id === link.target._id))
       if (linked != null) return 1
 
       return 0.2
     },
     zoomed () {
+      this.showLines = false
       this.zoom = d3.event.transform.k
       this.transform = `translate(${d3.event.transform.x} ${d3.event.transform.y}) scale(${d3.event.transform.k})`
     },
     zoomended () {
+      this.showLines = true
       if (d3.event.sourceEvent != null) return
-      console.log('ended')
+      // console.log('ended')
     },
     focus (node) {
       this.focused = node
 
+      console.log(node)
       if (this.settings.autoZoom === 'off') {
         this.clicked = true
       } else {
+        let linkedLinks = this.focused == null ? [] : this.links.filter((link) => this.focused._id === link.target._id || this.focused._id === link.source._id)
         this.nodes.forEach(n => {
-          n.opacity = this.checkOpacity(n)
+          n.opacity = this.checkOpacity(n, linkedLinks)
         })
         d3.select(this.$refs.all).transition().duration(this.settings.transition).call(this.d3Zoom.transform, d3.zoomIdentity.translate(this.width / 2 - node.x * this.settings.maxZoom, this.height / 2 - node.y * this.settings.maxZoom).scale(this.settings.maxZoom))
         // this.simulation.force('center', d3.forceCenter(node.x, node.y))
       }
     },
     resetZoom () {
-      let zoom = this.settings.minZoom
+      let zoom = 1.8
 
-      if (this.settings.autoMinZoom) {
-        let bbox = this.$refs.group.getBBox()
-
-        let maxX = Math.max(-bbox.x + this.width / 2, bbox.x + bbox.width - this.width / 2)
-        let maxY = Math.max(-bbox.y + this.height / 2, bbox.y + bbox.height - this.height / 2)
-
-        let zoomX = (this.width / 2) / (maxX)
-        let zoomY = (this.height / 2) / (maxY)
-
-        zoom = Math.min(zoomX, zoomY) * 0.95
-      }
+      // if (this.settings.autoMinZoom) {
+      //   let bbox = this.$refs.group.getBBox()
+      //
+      //   let maxX = Math.max(-bbox.x + this.width / 2, bbox.x + bbox.width - this.width / 2)
+      //   let maxY = Math.max(-bbox.y + this.height / 2, bbox.y + bbox.height - this.height / 2)
+      //
+      //   let zoomX = (this.width / 2) / (maxX)
+      //   let zoomY = (this.height / 2) / (maxY)
+      //
+      //   zoom = Math.min(zoomX, zoomY) * 0.95
+      // }
 
       d3.select(this.$refs.all).transition().duration(this.settings.transition).call(this.d3Zoom.transform, d3.zoomIdentity.translate(this.width / 2 - this.width / 2 * zoom, this.height / 2 - this.height / 2 * zoom).scale(zoom))
 
@@ -204,54 +224,20 @@ export default {
           this.nodes.forEach(n => {
             n.opacity = 1
           })
-          this.focused = null
+          // this.focused = null
+          this.showLines = true
         }, 1000)
-        this.d3Zoom.on('end', null)
+        this.d3Zoom.on('end', () => {
+          this.showLines = true
+        })
       })
     },
-    zoomIn () {
-      let statements = this.elNodes.filter(n => n.type === 'statement')
-      let statement = statements[Math.floor(statements.length * Math.random())]
-      if (Math.random() < 0.1) {
-        let filtered = statements.filter(s => {
-          return s.time > new Date().getTime() - 1000 * 60 * 5
-        })
-        console.log('-- new')
-        if (filtered.length) {
-          statement = filtered[Math.floor(filtered.length * Math.random())]
-          console.log('---- success')
-        }
-      } else if (Math.random() < 0.0) {
-        let filtered = statements.filter(s => s.controversityNorm > 0.8)
-        console.log('-- controverse')
-        if (filtered.length) {
-          statement = filtered[Math.floor(filtered.length * Math.random())]
-          console.log('---- success')
-        }
-      } else {
-        console.log('-- random')
-      }
-
-      if (this.lastFocus === statement._id) {
-        statement = statements[Math.floor(statements.length * Math.random())]
-      }
-
-      this.focus(statement)
-      setTimeout(() => {
-        this.zoomOut()
-      }, this.settings.wait + this.settings.transition)
-    },
-    zoomOut () {
-      if (this.focused) {
-        this.lastFocus = this.focused._id
-      }
-      this.resetZoom()
-      if (this.settings.autoZoom !== 'off') {
-        setTimeout(() => {
-          this.zoomIn()
-        }, 2000 + this.settings.transition + Math.random() * this.settings.autoZoom === 'slow' ? (1000 * 60 * 5) : this.settings.autoZoom === 'medium' ? (1000 * 45) : (1000 * 5))
-      }
-    }
+    throttleZoom:
+      _.throttle((_this) => {
+        // console.log(_this.throttledZoom)
+        _this.throttledZoom = _this.zoom
+      }, 20)
+    // }
   },
   computed: {
     simulation () {
@@ -266,7 +252,7 @@ export default {
       return d3.forceSimulation()
         .force('link', d3.forceLink()
           .id(function (d) { return d._id })
-          .strength(d => d.type === 'like' ? 1 : d.type === 'dislike' ? 0.1 : 0.5)
+          .strength(d => d.type === 'like' ? 1 : d.type === 'dislike' ? 0.01 : 0.5)
           .distance(70)
         )
         // .force('collide',
@@ -294,9 +280,12 @@ export default {
           // .distanceMin([1])
         )
         // .force('center', d3.forceCenter(this.width / 2, this.height / 2))
-        .on('tick', this.ticked)
+        // .on('tick', this.ticked)
         // .on("tick", this.tick)
         // .force('collide', collide)
+    },
+    strokeWidth () {
+      return 1 / this.throttledZoom
     }
     // weightScale () {
     //   return d3.scaleLinear()
@@ -320,11 +309,9 @@ export default {
 
       // console.log('updateupdateupdateupdate')
     },
-    'settings.autoZoom' (now, before) {
-      console.log('now:', now)
-      if (before === 'off') {
-        this.zoomIn()
-      }
+    zoom () {
+      // console.log('here')
+      this.throttleZoom(this)
     }
   }
 }
@@ -343,8 +330,13 @@ svg {
     pointer-events: fill;
     // stroke: #fff;
   }
+  .user-circle-filled {
+    pointer-events: fill;
+    stroke: none;
+  }
   .statement-circle {
     // fill: $YELLOW;
+    cursor: pointer;
   }
   .text {
     &.right-align {
@@ -352,6 +344,7 @@ svg {
     }
   }
   text {
+    pointer-events: none;
     font: $REGULAR;
     fill: #FFF;
     // text-anchor: middle;
@@ -362,6 +355,7 @@ svg {
     }
   }
   line, path {
+    pointer-events: none;
     opacity: .3;
     // stroke-width: 1;
     &.like {
@@ -374,8 +368,8 @@ svg {
       stroke: $PINK;
     }
   }
-  line, path, text, circle {
-    transition: opacity 1s;
+  path, text, circle {
+    transition: opacity 0.4s;
   }
 }
 .focus {
@@ -386,22 +380,24 @@ svg {
   height: 100%;
   top: 0px;
   left: 0px;
-  background: rgba(0,0,0,.3);
+  background: rgba(0,0,0,.5);
   // pointer-events: none;
   font: $REGULAR;
   color: $PINK;
 
   .text {
+    pointer-events: none;
     position: absolute;
     left: 50%;
     top: 50%;
 
     line-height: normal;
-    font-size: 56px;
+    font-size: 32px;
     transform: translate(-50%, -50%);
-    max-width: 900px;
+    max-width: 540px;
     width: 100%;
 
+    text-align: center;
     text-shadow: 0px 0px 12px black;
 
     .author {
--- client/views/Viz.vue
+++ client/views/Viz.vue
@@ -1,21 +1,13 @@
 <template>
   <div class="page">
-    <!-- <list :statements="statements" :user="user"></list> -->
-    <!-- <div class="time">
-      <div class="stamp">
-        {{time}}
-        {{new Date(+time).toLocaleTimeString()}}
-      </div>
-      <input class="slider" type="range" :max="new Date('08-22-2017').getTime()" :min="new Date('08-21-2017').getTime()" v-model="time">
-    </div> -->
     <network v-if="links.length" :links="links" :nodes="nodes" :settings="settings"></network>
   </div>
 </template>
 
 <script>
-import List from 'components/List'
 import Network from 'components/Network'
-import fetch from 'unfetch'
+import data from 'data.json'
+// import fetch from 'unfetch'
 import * as d3 from 'd3'
 
 export default {
@@ -31,7 +23,6 @@ export default {
     }
   },
   components: {
-    List,
     Network
   },
   created () {
@@ -39,104 +30,103 @@ export default {
   },
   methods: {
     update () {
-      fetch(`${this.$store.state.api}/all`) // http://psz.ft0.ch/api/all
-        .then(res => res.json())
-        .then(data => {
-          this.settings = data.settings
+      // fetch(`${this.$store.state.api}/all`) // http://psz.ft0.ch/api/all
+      //   .then(res => res.json())
+      //   .then(data => {
+      this.settings = data.settings
 
-          // data.statements = data.statements.filter(s => s.time <= this.time)
-          // data.likes = data.likes.filter(l => l.time <= this.time)
+      // data.statements = data.statements.filter(s => s.time <= this.time)
+      // data.likes = data.likes.filter(l => l.time <= this.time)
 
-          data.statements.forEach((statement, i) => {
-            statement.likes = data.likes.filter(like => like.statement === statement._id && like.isLike).length
-            statement.dislikes = data.likes.filter(like => like.statement === statement._id && !like.isLike).length
-            statement.ratings = statement.likes + statement.dislikes
-            statement.rating = statement.ratings > 0 ? statement.likes / statement.ratings : 0.5
-            statement.controversity = ((0.5 - Math.abs(statement.rating - 0.5)) * Math.pow(statement.ratings, 0.5))
-          })
-          let maxControversity = Math.max(...data.statements.map(s => s.controversity))
+      data.statements.forEach((statement, i) => {
+        statement.likes = data.likes.filter(like => like.statement === statement._id && like.isLike).length
+        statement.dislikes = data.likes.filter(like => like.statement === statement._id && !like.isLike).length
+        statement.ratings = statement.likes + statement.dislikes
+        statement.rating = statement.ratings > 0 ? statement.likes / statement.ratings : 0.5
+        statement.controversity = ((0.5 - Math.abs(statement.rating - 0.5)) * Math.pow(statement.ratings, 0.5))
+      })
+      let maxControversity = Math.max(...data.statements.map(s => s.controversity))
 
-          data.user.forEach(user => {
-            user.statements = data.statements.filter(statement => statement.user === user._id).length
-          })
-          let maxStatements = Math.max(...data.user.map(s => s.statements))
+      data.user.forEach(user => {
+        user.statements = data.statements.filter(statement => statement.user === user._id).length +
+          data.likes.filter(like => like.user === user._id).length / 3
+      })
+      let maxStatements = Math.max(...data.user.map(s => s.statements))
 
-          this.nodes.forEach(node => {
-            if (node.type === 'statement') {
-              let statement = data.statements.find(s => `s_${s._id}` === node._id)
-              node.controversityNorm = Math.max(statement.controversity / maxControversity, 0.125)
-              node.rating = statement.rating
-              node.fill = this.scale(statement.rating)
-            } else {
-              let user = data.user.find(u => `u_${u._id}` === node._id)
-              node.r = Math.max(user.statements / maxStatements, 0.25)
-            }
-          })
+      this.nodes.forEach(node => {
+        if (node.type === 'statement') {
+          let statement = data.statements.find(s => `s_${s._id}` === node._id)
+          node.controversityNorm = Math.max(statement.controversity / maxControversity, 0.125)
+          node.rating = statement.rating
+          node.fill = this.scale(statement.rating)
+        } else {
+          let user = data.user.find(u => `u_${u._id}` === node._id)
+          node.r = Math.max(user.statements / maxStatements, 0.25)
+        }
+      })
 
-          // console.log(...data.statements
-          //   .filter(s => this.nodes.find(node => node._id === 's_' + s._id) == null))
+      // console.log(...data.statements
+      //   .filter(s => this.nodes.find(node => node._id === 's_' + s._id) == null))
 
-          this.nodes.push(
-            ...data.statements
-              .filter(s => this.nodes.find(node => node._id === 's_' + s._id) == null)
-              .map(s => {
-                let user = data.user.find(u => u._id === s.user)
-                return {
-                  _id: 's_' + s._id,
-                  type: 'statement',
-                  text: s.text,
-                  controversityNorm: Math.max(s.controversity / maxControversity, 0.125),
-                  rating: s.rating,
-                  fill: this.scale(s.rating),
-                  author: user && user.name ? user.name : '',
-                  time: s.time,
-                  ratio: `${s.likes} / ${s.dislikes}`,
-                  opacity: 1
-                }
-              }),
-            ...data.user
-              .filter(u => (data.statements.find(s => s.user === u._id) != null) || (data.likes.find(s => s.user === u._id) != null))
-              .filter(u => this.nodes.find(node => node._id === 'u_' + u._id) == null)
-              .map(u => {
-                return {
-                  _id: 'u_' + u._id,
-                  type: 'user',
-                  text: u.name,
-                  r: Math.max(u.statements / maxStatements, 0.25),
-                  opacity: 1
-                }
-              })
-          )
-
-          // nodes.statements.find(d => s_59935065267f441bf0c44a78¿¿)
-
-          let likes = [...data.likes, ...data.statements.map(s => {
+      this.nodes.push(
+        ...data.statements
+          .filter(s => this.nodes.find(node => node._id === 's_' + s._id) == null)
+          .map(s => {
+            let user = data.user.find(u => u._id === s.user)
             return {
-              statement: s._id,
+              _id: 's_' + s._id,
+              type: 'statement',
+              text: s.text,
+              controversityNorm: Math.max(s.controversity / maxControversity, 0.125),
+              controversityNorm12: Math.max(s.controversity / maxControversity, 0.125) * 12,
+              rating: s.rating,
+              fill: this.scale(s.rating),
+              author: user && user.name ? user.name : '',
               time: s.time,
-              user: s.user,
-              _id: s.user + s._id
+              ratio: `${s.likes} / ${s.dislikes}`,
+              opacity: 1
             }
-          })]
+          }),
+        ...data.user
+          .filter(u => (data.statements.find(s => s.user === u._id) != null) || (data.likes.find(s => s.user === u._id) != null))
+          .filter(u => this.nodes.find(node => node._id === 'u_' + u._id) == null)
+          .map(u => {
+            return {
+              _id: 'u_' + u._id,
+              type: 'user',
+              text: u.name,
+              r: Math.max(u.statements / maxStatements, 0.125),
+              fontSize: Math.max(u.statements / maxStatements, 0.25),
+              engaged: (u.statements / maxStatements) > 0.25,
+              opacity: 1
+            }
+          })
+      )
 
-          // let likes = [...data.likes]
+      // nodes.statements.find(d => s_59935065267f441bf0c44a78¿¿)
 
-          this.links.push(...likes
-            .filter(like => this.links.find(link => link._id === like._id) == null)
-            .map(like => {
-              return {
-                _id: like._id,
-                source: 'u_' + like.user,
-                target: 's_' + like.statement,
-                type: like.isLike === true ? 'like' : like.isLike === false ? 'dislike' : 'author'
-              }
-            })
-          )
-          // .filter(link => link.type !== 'dislike')
-          setTimeout(() => {
-            this.update()
-          }, 10000)
+      let likes = [...data.likes, ...data.statements.map(s => {
+        return {
+          statement: s._id,
+          time: s.time,
+          user: s.user,
+          _id: s.user + s._id
+        }
+      })]
+
+      // let likes = [...data.likes]
+
+      this.links.push(...likes
+        .filter(like => this.links.find(link => link._id === like._id) == null)
+        .map(like => {
+          return {
+            _id: like._id,
+            source: 'u_' + like.user,
+            target: 's_' + like.statement,
+            type: like.isLike === true ? 'like' : like.isLike === false ? 'dislike' : 'author'
+          }
         })
+      )
     }
   }
 }
